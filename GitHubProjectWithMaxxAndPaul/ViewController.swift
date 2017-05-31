8fd9acf179702d2afc707863437df22487304e30
hcaelxxam <maxxleach@Yung-and-Tims-MacBook-Pro-2016.local>
2017-05-31 19:42:36 -0400

    Maxx’s Commit

---
+++ GitHubProjectWithMaxxAndPaul/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitHubProjectWithMaxxAndPaul
+//
+//  Created by Yung Dai on 2017-05-31.
+//  Copyright © 2017 Yung Dai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
